9b2194ef85cc3b339e17234162e7738fac4901cb
Brian Butterfield <brian@butter-field.net>
2021-02-03 10:07:44 -0700

    Initial commit.

---
+++ FormsConcept/FormsConcept/NotePage.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using FormsConcept.Model;
+using Xamarin.Forms;
+
+namespace FormsConcept
+{
+    
+    public class NotePage : ContentPage
+    {
+        private Editor _noteEditor;
+
+        public NotePage()
+        {
+            
+
+            Title = "Note";
+
+            Label noteLabel = new Label
+            {
+                Text = "Note",
+                HorizontalOptions = LayoutOptions.Center,
+                FontAttributes = FontAttributes.Bold,
+            };
+
+            _noteEditor = new Editor();
+            _noteEditor.Placeholder = "Enter Note Text";
+            _noteEditor.HeightRequest = 100;
+            _noteEditor.SetBinding(Editor.TextProperty, "Text");
+
+            Grid noteButtonGrid = new Grid
+            {
+                ColumnDefinitions =
+                {
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) },
+                },
+            };
+
+            Button saveButton = new Button();
+            saveButton.Text = "Save";
+            saveButton.Clicked += OnSaveButton_Clicked;
+
+            Button deleteButton = new Button();
+            deleteButton.Text = "Delete";
+            deleteButton.Clicked += OnDeleteButton_Clicked;
+
+            noteButtonGrid.Children.Add(saveButton);
+            noteButtonGrid.Children.Add(deleteButton, 1, 0);
+
+            Content = new StackLayout()
+            {
+                Margin = new Thickness(10, 35, 10, 10),
+
+                Children =
+                {
+                    noteLabel,
+                    _noteEditor,
+                    noteButtonGrid
+                }
+            };
+        }
+
+        private async void OnSaveButton_Clicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (string.IsNullOrWhiteSpace(note.Filename))
+            {
+                // Save.
+                var filename = Path.Combine(App.FolderPath, $"{Path.GetRandomFileName()}.notes.txt");
+                File.WriteAllText(filename, note.Text);
+            }
+            else
+            {
+                // Update.
+                File.WriteAllText(note.Filename, note.Text);
+            }
+
+            await Navigation.PopAsync();
+        }
+
+        private async void OnDeleteButton_Clicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+
+            if (File.Exists(note.Filename) == true)
+            {
+                File.Delete(note.Filename);
+            }
+
+            await Navigation.PopAsync();
+        }
+    }
+}
---
+++ FormsConcept/FormsConcept/NotesPage.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using FormsConcept.Model;
+using Xamarin.Forms;
+
+namespace FormsConcept
+{
+    public class NotesPage : ContentPage
+    {
+        private ListView _notesListView;
+
+        public NotesPage()
+        {
+            Title = "Notes";
+
+            ToolbarItem toolbarAdd = new ToolbarItem();
+            toolbarAdd.Text = "+";
+            toolbarAdd.Clicked += OnNoteAdd_Clicked;
+
+            ToolbarItems.Add(toolbarAdd);
+
+
+            _notesListView = new ListView();
+
+            _notesListView.Margin = new Thickness(20);
+            _notesListView.ItemSelected += OnNoteSelected;
+
+            _notesListView.ItemTemplate = new DataTemplate(() =>
+            {
+                TextCell textCell = new TextCell();
+                textCell.SetBinding(TextCell.TextProperty, "Text");
+                textCell.SetBinding(TextCell.DetailProperty, "Date");
+
+                return textCell;
+ 
+            });
+
+            Content = _notesListView;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var notes = new List<Note>();
+
+            var files = Directory.EnumerateFiles(App.FolderPath, "*.notes.txt");
+
+            // Iterate through disk files and populate the Note List.
+            foreach (var filename in files)
+            {
+                notes.Add(new Note
+                {
+                    Filename = filename,
+                    Text = File.ReadAllText(filename),
+                    Date = File.GetCreationTime(filename)
+                });
+            }
+
+            _notesListView.ItemsSource = notes
+                .OrderBy(d => d.Date)
+                .ToList();
+        }
+
+        private async void OnNoteAdd_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new NotePage
+            {
+                BindingContext = new Note()
+            });
+        }
+
+        private async void OnNoteSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new NotePage
+                {
+                    BindingContext = e.SelectedItem as Note
+                });
+            }
+        }
+    }
+}
+
